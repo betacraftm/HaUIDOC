31c68e79a4bbb620c07f5098eea8f93a8c761078
betacraftm <hoangkimc9@gmail.com>
2025-09-21 22:06:33 +0700

    refactor: add try/catch for server action

--- app/lib/data.js
+++ app/lib/data.js
@@ -105,26 +105,36 @@ export const checkDocumentExcist = async (docId) => {
 };
 
 export const getDocumentById = async (docId) => {
-  const document = await prisma.document.findFirst({
-    where: { id: docId },
-    include: {
-      users: { select: { id: true, name: true, image_url: true } },
-    },
-    omit: { uploaded_by: true, subject_id: true },
-  });
+  try {
+    const document = await prisma.document.findFirst({
+      where: { id: docId },
+      include: {
+        users: { select: { id: true, name: true, image_url: true } },
+      },
+      omit: { uploaded_by: true, subject_id: true },
+    });
 
-  return document;
+    return document;
+  } catch (error) {
+    console.error("Error get document by id:", error);
+    return null;
+  }
 };
 
 export const getComments = async (docId) => {
-  const comments = await prisma.comment.findMany({
-    where: { document_id: docId },
-    include: { users: { select: { name: true, image_url: true } } },
-    omit: { document_id: true, user_id: true },
-    orderBy: { created_at: "desc" },
-  });
+  try {
+    const comments = await prisma.comment.findMany({
+      where: { document_id: docId },
+      include: { users: { select: { name: true, image_url: true } } },
+      omit: { document_id: true, user_id: true },
+      orderBy: { created_at: "desc" },
+    });
 
-  return comments;
+    return comments;
+  } catch (error) {
+    console.error("Error get comments:", error);
+    return null;
+  }
 };
 
 export const getDocuments = async (section, page, userId) => {
@@ -145,61 +155,76 @@ export const getDocuments = async (section, page, userId) => {
       return await getLikedDocuments(page, userId);
   }
 
-  const documents = await prisma.document.findMany({
-    skip: (page - 1) * PAGE_SIZE,
-    take: PAGE_SIZE,
-    where: query.where,
-    orderBy: query.orderBy,
-    include: { subjects: { select: { name: true } } },
-  });
+  try {
+    const documents = await prisma.document.findMany({
+      skip: (page - 1) * PAGE_SIZE,
+      take: PAGE_SIZE,
+      where: query.where,
+      orderBy: query.orderBy,
+      include: { subjects: { select: { name: true } } },
+    });
 
-  const total = await prisma.document.count(query.where);
+    const total = await prisma.document.count(query.where);
 
-  return { documents, total };
+    return { documents, total };
+  } catch (error) {
+    console.error("Error get documents:", error);
+    return null;
+  }
 };
 
 const getViewedDocuments = async (page, userId) => {
-  const documents = await prisma.userViewedDocument.findMany({
-    skip: (page - 1) * PAGE_SIZE,
-    take: PAGE_SIZE,
-    orderBy: { viewed_at: "desc" },
-    where: { user_id: userId },
-    include: {
-      document: { include: { subjects: { select: { name: true } } } },
-    },
-  });
+  try {
+    const documents = await prisma.userViewedDocument.findMany({
+      skip: (page - 1) * PAGE_SIZE,
+      take: PAGE_SIZE,
+      orderBy: { viewed_at: "desc" },
+      where: { user_id: userId },
+      include: {
+        document: { include: { subjects: { select: { name: true } } } },
+      },
+    });
 
-  const total = await prisma.userViewedDocument.count({
-    where: { user_id: userId },
-  });
+    const total = await prisma.userViewedDocument.count({
+      where: { user_id: userId },
+    });
 
-  return {
-    documents: documents.map((doc) => ({
-      ...doc.document,
-      viewed_at: doc.viewed_at,
-    })),
-    total,
-  };
+    return {
+      documents: documents.map((doc) => ({
+        ...doc.document,
+        viewed_at: doc.viewed_at,
+      })),
+      total,
+    };
+  } catch (error) {
+    console.error("Error get viewed documents:", error);
+    return null;
+  }
 };
 
 const getLikedDocuments = async (page, userId) => {
-  const documents = await prisma.userLikedDocument.findMany({
-    skip: (page - 1) * PAGE_SIZE,
-    take: PAGE_SIZE,
-    where: { user_id: userId },
-    include: {
-      document: { include: { subjects: { select: { name: true } } } },
-    },
-  });
+  try {
+    const documents = await prisma.userLikedDocument.findMany({
+      skip: (page - 1) * PAGE_SIZE,
+      take: PAGE_SIZE,
+      where: { user_id: userId },
+      include: {
+        document: { include: { subjects: { select: { name: true } } } },
+      },
+    });
 
-  const total = await prisma.userLikedDocument.count({
-    where: { user_id: userId },
-  });
+    const total = await prisma.userLikedDocument.count({
+      where: { user_id: userId },
+    });
 
-  return {
-    documents: documents.map((doc) => ({
-      ...doc.document,
-    })),
-    total,
-  };
+    return {
+      documents: documents.map((doc) => ({
+        ...doc.document,
+      })),
+      total,
+    };
+  } catch (error) {
+    console.error("Error get liked documents:", error);
+    return null;
+  }
 };
