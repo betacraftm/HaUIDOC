7bcdc00c7d9b36022bbaf2f08eff05ece2e85a95
betacraftm <hoangkimc9@gmail.com>
2025-12-30 23:17:37 +0700

    update ui for home page

--- components/HomeSection.jsx
+++ components/HomeSection.jsx
@@ -3,34 +3,36 @@ import Link from "next/link";
 
 const HomeSection = ({ title, data = [], viewAllString, metaData, href }) => {
   return (
-    <section className="mb-10">
-      <h2 className="mb-6 text-2xl font-bold text-gray-800">{title}</h2>
-      {data.length != 0 ? (
-        <div className="hide-scrollbar grid grid-cols-1 overflow-x-auto">
-          {data.map((doc) => (
-            <div key={doc.id}>
-              <DocumentCard
-                title={doc.title}
-                subject={doc?.subjects.name}
-                linkUrl={`/documents/${doc.id}`}
-                metaData={metaData}
-                doc={doc}
-              />
+    <section>
+      {data.length != 0 && (
+        <div className="mb-10">
+          <h2 className="mb-6 text-2xl font-bold text-gray-800">{title}</h2>
+          {
+            <div className="hide-scrollbar grid grid-cols-1 overflow-x-auto">
+              {data.map((doc) => (
+                <div key={doc.id}>
+                  <DocumentCard
+                    title={doc.title}
+                    subject={doc?.subjects.name}
+                    linkUrl={`/documents/${doc.id}`}
+                    metaData={metaData}
+                    doc={doc}
+                  />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      ) : (
-        <p>Chưa có tài liệu nào</p>
-      )}
+          }
 
-      {data.length >= 5 && (
-        <div className="mt-8 text-center">
-          <Link
-            href={href}
-            className="text-primary font-semibold hover:underline"
-          >
-            {viewAllString} &rarr;
-          </Link>
+          {data.length >= 5 && (
+            <div className="mt-8 text-center">
+              <Link
+                href={href}
+                className="text-primary font-semibold hover:underline"
+              >
+                {viewAllString} &rarr;
+              </Link>
+            </div>
+          )}
         </div>
       )}
     </section>
